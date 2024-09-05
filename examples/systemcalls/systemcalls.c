0ccfecc2a0e8783e5230b6e03f7ced2f5b96f8fa
AdminAdam <AdminAdam@github.com>
2024-09-05 15:19:09 +0200

    Assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,8 +22,9 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int success = system(cmd);
 
-    return true;
+    return (success != -1);
 }
 
 /**
@@ -45,9 +52,6 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
 /*
  * TODO:
@@ -58,10 +62,31 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
+    pid_t pid = fork();
+    if (pid == -1 ) {
+        return false;
+    } else if(pid == 0 ){
+        status = execv(command[0], command);
+        if (status == -1)
+            exit(-1);
+    }
+
+    if (waitpid(pid, &status, 0) == -1 ) {
+        return false;
+    }
 
     va_end(args);
+    
+    if(!WEXITSTATUS(status))
+    {
+        return true; 
+    }
 
-    return true;
+    else
+    {
+        return false;
+    }
 }
 
 /**
@@ -92,8 +117,38 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    int status;
+    if (fd < 0) {
+        return false;
+    }
+    pid_t pid = fork();
+    if (pid == -1 ) {
+        return false;
+    } else if(pid == 0 ){
+        if (dup2(fd, 1) < 0) {
+            close(fd);
+            exit(-1);
+        }
+        
+        status = execv(command[0], command);
+        if (status == -1)
+            exit(-1);
+    }
+
+    if (waitpid(pid, &status, 0) == -1 ) {
+        return false;
+    }
 
     va_end(args);
+    
+    if(!WEXITSTATUS(status))
+    {
+        return true; 
+    }
 
-    return true;
+    else
+    {
+        return false;
+    }
 }
